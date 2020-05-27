67e7aa7c4e3f5926643deb64fa6cad11362ffb10
unknown <ryanklueg@gmail.com>
2020-05-26 22:48:20 -0400

    added death layer and layer controls

--- docs/static/js/heatmap.js
+++ docs/static/js/heatmap.js
@@ -1,25 +1,9 @@
-//Set-up map
-const myMap = L.map("infection-heatmap", {
-    center: [39.50, -98.35],
-    zoom: 5
-  });
-
-//Add background layer to heatmap
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-maxZoom: 18,
-id: "streets-v9",
-accessToken: API_KEY
-}).addTo(myMap)
-
-//Access infection data for heatmap
 d3.csv("data/test_data.csv", infectionData => {
     //Parse through data
     infectionData.forEach(d => {
         d.cases = +d.cases;
         d.deaths = +d.deaths;
     });
-    console.log(infectionData);
     
     //Create Array for heatmap infection layer
     const infectionArr = [];
@@ -41,9 +25,46 @@ d3.csv("data/test_data.csv", infectionData => {
         }
     }
 
+    //Create layers for infections and deaths
     //Heatmap layer for infection rates
-    const infection = L.heatLayer(infectionArr, {
+    const infectionLayer = L.heatLayer(infectionArr, {
         radius: 20,
         blur: 35
-      }).addTo(myMap);
+      })
+    //Heatmap layer for death rates
+    const deathLayer = L.heatLayer(deathArr, {
+        radius: 20,
+        blur: 35,
+        gradient: {
+            .2: "green",
+            .4: "yellow",
+            .6: "red",
+            .8: "grey",
+            1: "black"
+        }
+    })
+
+    //Set Overlay Maps
+    const overlayMaps = {
+        Infections: infectionLayer,
+        Deaths: deathLayer
+    };
+
+    //Set-up map
+    const myMap = L.map("infection-heatmap", {
+        center: [39.50, -98.35],
+        zoom: 5,
+        layers: [infectionLayer]
+    });
+
+    //Add background layer to heatmap
+    L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "streets-v9",
+        accessToken: API_KEY
+        }).addTo(myMap)
+
+    //Add Layer control
+    L.control.layers(overlayMaps).addTo(myMap);
 });
