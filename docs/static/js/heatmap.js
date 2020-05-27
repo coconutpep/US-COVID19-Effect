7f6cb9b31c31ab9fd89cc64ac59b89ca2e7b3235
unknown <ryanklueg@gmail.com>
2020-05-26 21:33:56 -0400

    test heatmap

--- docs/static/js/heatmap.js
+++ docs/static/js/heatmap.js
@@ -13,8 +13,37 @@ accessToken: API_KEY
 }).addTo(myMap)
 
 //Access infection data for heatmap
-d3.csv("data/covid_confirmed_usafacts.csv").then(infectionData => {
+d3.csv("data/test_data.csv", infectionData => {
+    //Parse through data
+    infectionData.forEach(d => {
+        d.cases = +d.cases;
+        d.deaths = +d.deaths;
+    });
+    console.log(infectionData);
+    
+    //Create Array for heatmap infection layer
+    const infectionArr = [];
+    //Create Array for heatmap death layer
+    const deathArr = [];
+    //Iterate through data and append counties
+    for (let i=0; i<infectionData.length; i++) {
+        //Create multipliers from number of cases and deaths
+        const cases = infectionData[i].cases;
+        const deaths = infectionData[i].deaths;
+        //Create for loops using "multipliers" to append coords for every case and death
+        //For loop for cases
+        for (let j=1; j<=cases; j++) {
+            infectionArr.push([infectionData[i].lat, infectionData[i].long]);
+        }
+        //For loop for deaths
+        for (let k=1; k<=deaths; k++) {
+            deathArr.push([infectionData[i].lat, infectionData[i].long]);
+        }
+    }
 
-    //
-
+    //Heatmap layer for infection rates
+    const infection = L.heatLayer(infectionArr, {
+        radius: 20,
+        blur: 35
+      }).addTo(myMap);
 });
