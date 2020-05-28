8c55c91f89eade434d87413e519193c085aa0ec0
unknown <ryanklueg@gmail.com>
2020-05-28 18:33:37 -0400

    set event handler to change heatmap based on date input

--- docs/static/js/heatmap-plot.js
+++ docs/static/js/heatmap-plot.js
@@ -1,19 +1,32 @@
+//Create selector for date input
+const dateInput = d3.select("#date-input");
+
+//function to convert data date to date object
+const parseTime = d3.timeParse("%m/%e/%y");
+//function to format date object to input date format
+const formatTime = d3.timeFormat("%Y-%m-%d");
+
 //Read in infection & death data
-d3.csv("data/test_data.csv", infectionData => {
+d3.csv("data/county_clean.csv", infectionData => {
     //Parse through data
     infectionData.forEach(d => {
+        d.date = formatTime(parseTime(d.date));
         d.cases = +d.cases;
         d.deaths = +d.deaths;
     });
-    //Set data
+
+    //Create datasets for heatmap
     const testDataInfection = {
+        min: d3.min(infectionData, d=> d.cases),
         max: d3.max(infectionData, d => d.cases),
         data: infectionData
     };
     const testDataDeath = {
+        min: d3.min(infectionData, d=> d.cases),
         max: d3.max(infectionData, d => d.deaths),
         data: infectionData
     };
+
     //Set base layer for the map
     const baseLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -66,4 +79,28 @@ d3.csv("data/test_data.csv", infectionData => {
 
     //Create Layer control
     L.control.layers(overlayMaps).addTo(myMap);
-})
+
+    //Function to redraw data based on date
+    function dataFilter() {
+        //Grab Date value
+        const dateValue = dateInput.property("value");
+        //Filter data based on date input
+        const dataFiltered = infectionData.filter(d => d.date === dateValue);
+        //Reset Heatmap Data
+        testDataInfection = {
+            min: d3.min(dataFiltered, d => d.cases),
+            max: d3.max(dataFiltered, d => d.cases),
+            data: dataFiltered
+        };
+        testDataDeath = {
+            min: d3.min(dataFiltered, d => d.deaths),
+            max: d3.max(dataFiltered, d => d.deaths),
+            data: dataFiltered
+        };
+        //Redraw heatmap layers
+        infectionLayer.setData(testDataInfection);
+        deathLayer.setData(testDataDeath);
+    }
+    //Event handler to change heatmap on user input
+    dateInput.on("change", dataFilter);
+});
