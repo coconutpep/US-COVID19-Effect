3c064b5b6d429fc43b498e05b775401f51e3e1f5
unknown <ryanklueg@gmail.com>
2020-06-03 14:14:27 -0400

    fixed bug caused by more than two comparisons

--- docs/static/js/chart-display.js
+++ docs/static/js/chart-display.js
@@ -4,38 +4,6 @@ const dateType = d3.select("#date-type");
 const dateSingle = d3.select("#date-input");
 const dateStart = d3.select("#start-date-holder");
 const dateEnd = d3.select("#end-date-holder");
-//Function to change input display for datetype
-function inputRender() {
-    //Grab date type value
-    const dateTypeValue = dateType.property("value");
-    //Switch statement to adjust display
-    switch (dateTypeValue) {
-        case "Date Range": 
-        {
-            dateSingle.style("display", "none");
-            dateStart.style("display", "");
-            dateEnd.style("display", "");
-            d3.select("#infection-heatmap").style("display", "none");
-            d3.select("#weather-heatmap").style("display", "none");
-            d3.select("#infection-line").style("display", "");
-            d3.select("#weather-line").style("display", "");
-            d3.select("#infection-heatmap").innerHTML = "";
-            d3.select("#weather-heatmap").innerHTML = "";
-            break;
-        }
-        case "Single Date":
-        { 
-            dateStart.style("display", "none");
-            dateEnd.style("display", "none");
-            dateSingle.style("display", "");
-            d3.select("#infection-line").style("display", "none");
-            d3.select("#weather-line").style("display", "none");
-            d3.select("#infection-heatmap").style("display", "");
-            d3.select("#weather-heatmap").style("display", "");
-            break;
-        }
-    }
-}
 
 //Selector for comparison chart
 const chartSelect = d3.select("#comparison");
@@ -50,6 +18,7 @@ const popDenClass = popDenClassSelector["_groups"][0];
 //Function to adjust website based on chart selection
 function renderChart() {
     const currentChart = d3.select("#comparison").property("value");
+    const dateTypeValue = dateType.property("value");
     //Conditional statements to set displays
     switch (currentChart) {
         case 'Weather Comparison':
@@ -75,6 +44,32 @@ function renderChart() {
                 dateEnd.select("input").attr("value", "2020-03-31");
                 dateEnd.select("input").attr("min", "2020-01-22");
                 dateEnd.select("input").attr("max", "2020-05-25");
+                dateType.property("disabled", false);
+                switch (dateTypeValue) {
+                    case 'Date Range':
+                        {
+                            dateSingle.style("display", "none")
+                            dateStart.style("display", "");
+                            dateEnd.style("display", "")
+                            d3.select("#infection-heatmap").style("display", "none");
+                            d3.select("#weather-heatmap").style("display", "none");
+                            d3.select("#infection-line").style("display", "inline-block");
+                            d3.select("#weather-line").style("display", "inline-block");
+                            break;
+                        }
+                    case 'Single Date':
+                        {
+                            dateStart.style("display", "none");
+                            dateEnd.style("display", "none");
+                            dateSingle.style("display", "inline-block");
+                            d3.select("#infection-line").style("display", "none");
+                            d3.select("#weather-line").style("display", "none");
+                            d3.select("#infection-heatmap").style("display", "inline-block");
+                            d3.select("#weather-heatmap").style("display", "inline-block");
+                            runInfection();
+                            break;
+                        }
+                }
                 break;
           }
         case 'Stock Lookup':
@@ -110,6 +105,7 @@ function renderChart() {
                 dateSingle.attr("value", "2020-05-25");
                 dateSingle.property("value", "2020-05-25");
                 runInfection();
+                break;
             }
     }
 }
@@ -118,8 +114,4 @@ function renderChart() {
 renderChart();
 //Event handler to change charts when selected
 chartSelect.on("change", renderChart);
-
-//Set initial input display
-inputRender();
-//Event handler to change input display
-dateType.on("change", inputRender);
+dateType.on("change", renderChart);
