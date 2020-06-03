20c60794b17ad2cda445cdfbf94fcd9c179291cd
unknown <ryanklueg@gmail.com>
2020-06-03 10:35:32 -0400

    added min and max date values based on comparison

--- docs/static/js/chart-display.js
+++ docs/static/js/chart-display.js
@@ -60,10 +60,16 @@ function renderChart() {
                 }
                 dateSingle.property("value", "2020-03-31");
                 dateSingle.attr("value", "2020-03-31");
+                dateSingle.attr("min", "2020-01-22");
+                dateSingle.attr("max", "2020-03-31");
                 dateStart.select("input").property("value", "2020-01-22");
                 dateStart.select("input").attr("value", "2020-01-22");
+                dateStart.select("input").attr("min", "2020-01-22");
+                dateStart.select("input").attr("max", "2020-03-31");
                 dateEnd.select("input").property("value", "2020-03-31");
                 dateEnd.select("input").attr("value", "2020-03-31");
+                dateEnd.select("input").attr("min", "2020-01-22");
+                dateEnd.select("input").attr("max", "2020-03-31");
                 break;
           }
         case 'Stock Lookup':
